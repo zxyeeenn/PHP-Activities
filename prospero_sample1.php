4b08b0fb0cc00ce663c0435d459231f91c3684b5
zxyeeenn <123625032+zxyeeenn@users.noreply.github.com>
2023-03-28 16:21:54 +0800

    Add files via upload

---
+++ prospero_sample1.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+
+
+</body>
+</html>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <center>
+        <?php
+            //Multiplier
+            $num = 5;
+
+            //Display
+            echo "<h1>Multiplication Table of .$num.<br></h2>";
+            for($x = 0; $x <= 10; $x++){
+
+                $z = $num * $x; 
+                echo "$num * $x = $z";
+                echo '<br>';
+        }
+        ?>
+    </center>
+</body>
+</html>
